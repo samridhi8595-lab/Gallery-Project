fb661140109aca91cb1b7bdc0341107c1bec51a8
samridhi8595-lab <samridhi8595@gmail.com>
2026-01-05 16:57:46 +0530

    Gallery project done

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,57 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import Card from './components/Card'
 
 const App = () => {
   const [userData, setUserData] = useState([])
-  const getData= async ()=>{
-  const response=await axios.get('https://picsum.photos/v2/list?page=3&limit=30')
-  setUserData(response.data)
-  console.log(response.data);
+  const [index, setIndex] = useState(1)
 
+  const getData= async ()=>{
+  const response=await axios.get(`https://picsum.photos/v2/list?page=${index}&limit=10`)
+  setUserData(response.data)
+  
   }
-  let printUserData = 'no user Available'
+  useEffect(function(){
+   getData()
+  },[index])
+
+  let printUserData = <h3 className='text-gray-300 text-xs absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 font-semibold'>Loading....</h3>
   if(userData.length>0){
     printUserData=userData.map(function(elem,idx){
       return <div key={idx}>
-       <a href={elem.url} target='_blank'>
-         <div className='h-40 w-44 bg-white rounded-xl'>
-        <img className='h-full w-full overflow-hidden object-cover'src={elem.download_url} alt="" />
-      </div>
-      <h2 className='font-bold text-lg'>{elem.author}</h2>
-       </a>
+       <Card elem={elem}/>
       </div>
     })
   }
 
   return (
     <div className='bg-black overflow-auto h-screen p-4 text-white'>
-      <button
-      onClick={getData}
-       className='bg-green-600 active:scale-95 mb-3 px-5 py-4 text-white'>Get data</button>
-       <div className='flex flex-wrap gap-4'>
+     
+       <div className='flex h-[82%] flex-wrap p-2 gap-4'>
         {printUserData}
+       </div>
+       <div className='flex justify-center gap-6 items-center p-4'>
+        <button
+        style={{opacity: index==1 ? 0.5 :1}}
+        className='bg-amber-400 text-sm cursor-pointer active:scale-95 text-black rounded px-4 py-2 font-semibold'
+        onClick={()=>{
+          if(index>1){
+            setIndex(index-1)
+            setUserData([])
+          }
+        }}
+        >
+          Prev
+        </button>
+        <h4>Page{index}</h4>
+        <button className='bg-amber-400 text-sm cursor-pointer active:scale-95 text-black rounded px-4 py-2 font-semibold'
+        onClick={()=>{
+         setIndex(index+1)
+         setUserData([])
+        }}
+        >
+          Next
+          </button>
        </div>
     </div>
   )
